39f7facd27e54f49534382ece933a4ec5c697b5a
Marroc722 <marroc722@ihmeducation.se>
2024-09-27 14:05:00 +0200

    Game

--- js/app.js
+++ js/app.js
@@ -4,11 +4,16 @@ const button1 = document.getElementById("button1");
 //TEXT
 const playerRollText = document.getElementById("playerRollText");
 const aiRollText = document.getElementById("aiRollText");
-
+const result = document.getElementById("result");
+const playerScoreText = document.getElementById("playerScoreText");
+const aiScoreText = document.getElementById("aiScoreText");
 
 // Variables
 let playerRollValue = 0;
 let aiRollValue = 0;
+let playerScore = 0;
+let aiScore = 0;
+
 
 //controllers
 function getRandomInt() {
@@ -24,6 +29,9 @@ function getRandomInt() {
 button1.addEventListener("click", function() {
   rollAiAndPlayer();
   showRollResults();
+  compareNumbers();
+  showPlayerResults();
+  showAiResult();
 });
 
 //Function
@@ -31,6 +39,27 @@ function rollAiAndPlayer (){
   playerRollValue = getRandomInt();
   aiRollValue = getRandomInt();
 }
+
+function compareNumbers () {
+  if (playerRollValue > aiRollValue) {
+    result.innerHTML = "Poäng till spelare";
+    playerScore++;
+  }
+  if (playerRollValue < aiRollValue) {
+    result.innerHTML = "Poäng till datorn";
+    aiScore++;
+  }
+  if (playerRollValue == aiRollValue) {
+    result.innerHTML = "oavgjort";
+
+  }
+}
+
+function showPlayerResults () {
+  playerScoreText.innerHTML = "Du har " + playerScore + "poäng";
+  aiScoreText.innerHTML = "Datorn har" + aiScore + "poäng";
+}
+
 
 // View
 function showRollResults (){
